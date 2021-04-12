b824443f480a0f528f3acc317ad50e78dca26f60
xuhaiqiang <xuhaiqiang@xiaoheiban.cn>
2021-04-12 19:04:32 +0800

    feat: add loader

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,16 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          path.resolve(__dirname, 'lib/loaders/remove-console-loader.js'),
+          path.resolve(__dirname, 'lib/loaders/add-author-loader.js')
+        ]
+      }
+    ]
+  },
 }
